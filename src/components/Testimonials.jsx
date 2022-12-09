c9d0c29042b5b7fcf391389f26e8540a67f155dc
Mustafa <mhassanzadeh777@gmail.com>
2022-12-08 18:34:56 -0700

    Modifed classnames for ease of reuse, added CSS for testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,7 +9,7 @@ const Testimonials = () => {
             <h1 className="testimonials-heading">CLIENT TESTIMONIALS</h1>
             <section className="testimonials-container">
                 <div className="testimonials-item">
-                    <img src={emily} alt="emily" className="testimonial-emily" />
+                    <img src={emily} alt="emily" className="testimonial-picture" />
                     <p className="testimonial-desc">
                         We put or trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.
                     </p>
@@ -17,7 +17,7 @@ const Testimonials = () => {
                     <p className='testimonial-signoff'>Marketing Director</p>
                 </div>
                 <div className="testimonials-item">
-                    <img src={thomas} alt="thomas" className='testimonial-thomas' />
+                    <img src={thomas} alt="thomas" className='testimonial-picture' />
                     <p className="testimonial-desc">
                         Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience. 
                     </p>
@@ -25,7 +25,7 @@ const Testimonials = () => {
                     <p className="testimonial-signoff">Chief Operating Officer</p>
                 </div>
                 <div className="testimonials-item">
-                    <img src={jennie} alt="jennie" className='testimonial-jennie' />
+                    <img src={jennie} alt="jennie" className='testimonial-picture' />
                     <p className="testimonial-desc">
                         Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
                     </p>
